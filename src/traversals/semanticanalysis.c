be3e348fa7a68dc600d547567b493b09d2bab6c8
julialfk <j.liemfk@gmail.com>
2023-03-14 19:00:55 +0100

    Added nodes and traversal for symbol table

---
+++ src/traversals/semanticanalysis.c
@@ -0,0 +1,105 @@
+/**
+ * @file
+ *
+ * Traversal: SemanticAnalysis
+ * UID      : SA
+ *
+ *
+ */
+
+#include "ccn/ccn.h"
+#include "ccngen/ast.h"
+
+void SAinit() { return; }
+void SAfini() { return; }
+
+/**
+ * @fn SAprogram
+ */
+node_st *SAprogram(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAglobdef
+ */
+node_st *SAglobdef(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAglobdecl
+ */
+node_st *SAglobdecl(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAfundef
+ */
+node_st *SAfundef(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAparam
+ */
+node_st *SAparam(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAvardecl
+ */
+node_st *SAvardecl(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAvar
+ */
+node_st *SAvar(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAsymtable
+ */
+node_st *SAsymtable(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAste
+ */
+node_st *SAste(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
+/**
+ * @fn SAvarlet
+ */
+node_st *SAvarlet(node_st *node)
+{
+    struct data_sa *data = DATA_SA_GET();
+    return node;
+}
+
---
+++ src/user_types.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "palm/hash_table.h"
+#include "ccngen/ast.h"
+
+
+typedef htable_st *htable_ptr;
+typedef node_st *node_ptr;
